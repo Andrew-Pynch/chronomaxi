103b44cd19577f948013c67023315590a6d5bf59
Andrew Pynch <andrewpynchbusiness@gmail.com>
2024-07-08 07:11:21 -0500

    frontend logs

--- frontend/src/app/api/public-stats/route.tsx
+++ frontend/src/app/api/public-stats/route.tsx
@@ -6,6 +6,8 @@ import { getActivityDataForCurrentUser } from "~/app/home/actions";
 export async function GET(request: NextRequest) {
     const activity = await getActivityDataForCurrentUser();
 
+    console.log("\n\nactivity", activity);
+
     return new Response(JSON.stringify(activity), {
         status: 200,
         headers: {
